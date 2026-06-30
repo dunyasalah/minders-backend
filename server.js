0faa916bde432f36ec1a68ff8cd88610ab0f0e1a
Dunya Salah <doniasalah945@gmail.com>
2026-06-30 09:35:52 +0300

    Fix Vercel configuration

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const dns = require("dns");
-dns.setServers(["8.8.8.8", "1.1.1.1"]); 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-require('dotenv').config();
+require("dotenv").config();
 
-console.log(process.env.MONGO_URI);
-
-const app = require('./src/app');
-const connectDatabase = require('./src/config/database');
+const app = require("./src/app");
+const connectDatabase = require("./src/config/database");
 
 const PORT = process.env.PORT || 5000;
 
-connectDatabase().then(() => {
+connectDatabase();
+
+if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
-});
+}
+
+module.exports = app;
